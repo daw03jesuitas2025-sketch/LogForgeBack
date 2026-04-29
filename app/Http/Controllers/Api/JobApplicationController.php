79afc984d5b00a5c2faf7726570df20a60ecabbe
Aliss <daw03.jesuitas.2025@gmail.com>
2026-04-29 11:51:23 +0200

    arreglando errores

--- app/Http/Controllers/Api/JobApplicationController.php
+++ app/Http/Controllers/Api/JobApplicationController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\JobApplication; // <--- ¡ESTO ES VITAL!
+use App\Models\JobApplication;
 use Illuminate\Support\Facades\Auth;
 
 class JobApplicationController extends Controller
